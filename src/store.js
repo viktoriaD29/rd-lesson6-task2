ff0dbab16490dbfad276f865e488bb55df95f5e1
Viktoria <doduchenko29@gmail.com>
2021-08-16 13:13:41 +0300

    add

--- src/store.js
+++ src/store.js
@@ -2,6 +2,15 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './users/users.reducer';
 
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
 const reducer = combineReducers({
   users: usersReducer,
 });
@@ -10,7 +19,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
 
 export default store;
